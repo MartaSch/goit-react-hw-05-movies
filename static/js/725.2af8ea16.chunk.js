edeae0b9396e87b5ad6522a037e8887ca18d141e
MartaSch <115217962+MartaSch@users.noreply.github.com>
2023-06-28 09:44:41 +0000

    Deploying to gh-pages from @ MartaSch/goit-react-hw-05-movies@7af153f9500b73644b536b848ae8d2bfec16a759 🚀

---
+++ static/js/725.2af8ea16.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[725],{725:function(e,n,r){r.r(n),r.d(n,{NotFound:function(){return c}});var t=r(87),o=r(184),c=function(){return(0,o.jsxs)(o.Fragment,{children:[(0,o.jsx)("h3",{children:"Something went wrong..."}),(0,o.jsx)(t.rU,{to:"/",children:"Home"}),(0,o.jsx)(t.rU,{to:"/movies",children:"Movies"})]})}}}]);
+//# sourceMappingURL=725.2af8ea16.chunk.js.map
